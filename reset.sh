460c5d42f21425173d54c819d23bc2741bdb7c88
Vibhu Chaudhary Patel <vibhuchaudharypatel.1997@gmail.com>
2017-10-27 01:50:06 -0400

    Create reset.sh

---
+++ reset.sh
@@ -0,0 +1,9 @@
+#remove the old files
+rm measure_malloc.txt
+rm measure_free.txt
+rm malloc_fail.txt
+
+#create new empty files
+touch measure_malloc.txt
+touch measure_free.txt
+touch malloc_fail.txt
